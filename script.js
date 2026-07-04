e6c3eff50d696391f8fef5da4c5b171b58700e0e
eugeniobr25 <eugeniobrilhante@hotmail.com>
2026-07-04 20:16:44 -0300

    aplicando Clean code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggleBtn = document.getElementById('theme-toggle');
     
-    // Verifica a preferência anterior salva pelo usuário
     const currentTheme = localStorage.getItem('theme');
     if (currentTheme === 'dark') {
         document.body.classList.add('dark-theme');
@@ -19,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
             themeToggleBtn.textContent = '🌙 Modo Escuro';
         }
         
-        // Persiste a escolha no navegador do usuário
         localStorage.setItem('theme', theme);
     });
 });
